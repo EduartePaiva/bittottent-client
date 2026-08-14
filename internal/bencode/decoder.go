c5871b67aa19fbc21d7d6be6585153e61810194c
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-08-13 21:07:20 -0300

    building readInt

--- internal/bencode/decoder.go
+++ internal/bencode/decoder.go
@@ -2,6 +2,8 @@ package bencode
 
 import (
 	"bufio"
+	"bytes"
+	"errors"
 	"io"
 	"strconv"
 )
@@ -9,6 +11,10 @@ import (
 type decoder struct {
 	bufio.Reader
 }
+
+var (
+	ErrLeadingZero = errors.New("error: int have leading zero")
+)
 
 func (d *decoder) readString() (string, error) {
 	bytesNum, err := d.ReadSlice(':')
@@ -30,4 +36,22 @@ func (d *decoder) readString() (string, error) {
 	}
 
 	return string(buf), nil
+}
+
+func (d *decoder) readInt() (int64, error) {
+	bytesInt, err := d.ReadSlice('e')
+	if err != nil {
+		return 0, err
+	}
+
+	// check leading zeros
+	if bytes.Equal(bytesInt, []byte("i-0e")) || (len(bytesInt) > 3 && bytesInt[1] == '0') {
+		return 0, ErrLeadingZero
+	}
+
+	strInt := string(bytesInt[1 : len(bytesInt)-1])
+
+	n, err := strconv.ParseInt(strInt, 10, 64)
+
+	return n, err
 }
