36048107022f1d78097e0ba60ed4b558a174c845
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-08-14 20:53:16 -0300

    building the readList decoder

--- internal/bencode/decoder.go
+++ internal/bencode/decoder.go
@@ -45,13 +45,62 @@ func (d *decoder) readInt() (int64, error) {
 	}
 
 	// check leading zeros
-	if bytes.Equal(bytesInt, []byte("i-0e")) || (len(bytesInt) > 3 && bytesInt[1] == '0') {
+	if bytes.Equal(bytesInt, []byte("-0e")) || (len(bytesInt) > 2 && bytesInt[1] == '0') {
 		return 0, ErrLeadingZero
 	}
 
-	strInt := string(bytesInt[1 : len(bytesInt)-1])
+	strInt := string(bytesInt[:len(bytesInt)-1])
 
 	n, err := strconv.ParseInt(strInt, 10, 64)
 
 	return n, err
+}
+
+func (d *decoder) readNextItem() (any, error) {
+	itemType, err := d.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+
+	switch itemType {
+	case 'i':
+		return d.readInt()
+	case 'l':
+		return d.readList()
+	default:
+		// default is a string type
+		err = d.UnreadByte()
+		if err != nil {
+			return nil, err
+		}
+
+		return d.readString()
+	}
+}
+
+func (d *decoder) readList() ([]any, error) {
+	// we need to read the next value until next byte is 'e'
+	var list []any = []any{}
+	for {
+		v, err := d.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		if v == 'e' {
+			break
+		}
+		err = d.UnreadByte()
+		if err != nil {
+			return nil, err
+		}
+
+		nextItem, err := d.readNextItem()
+		if err != nil {
+			return nil, err
+		}
+
+		list = append(list, nextItem)
+	}
+
+	return list, nil
 }
--- internal/bencode/decoder_test.go
+++ internal/bencode/decoder_test.go
@@ -30,3 +30,27 @@ func TestDecodeString(t *testing.T) {
 	}
 
 }
+
+func TestDecodeList(t *testing.T) {
+	tests := []struct {
+		name     string
+		sample   []byte
+		expected any
+	}{
+		{"empty list", []byte("le"), []any{}},
+		{"list of integers", []byte("li1ei2ei3ei123456789ee"), []any{int64(1), int64(2), int64(3), int64(123456789)}},
+		{"list of strings", []byte("l4:spam4:eggse"), []any{"spam", "eggs"}},
+		{"list of lists with integer and string", []byte("lli123e4:teste5:testei123ee"), []any{[]any{int64(123), "test"}, "teste", int64(123)}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			decoder := decoder{*bufio.NewReader(bytes.NewReader(tt.sample[1:]))}
+
+			list, err := decoder.readList()
+			t.Log(list)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.expected, list)
+		})
+	}
+}
