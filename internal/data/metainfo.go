bab86a41ef2516686ee2c41ec26ddfde5d9471af
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-09-17 20:22:12 -0300

    parsing some metainfo

--- internal/data/metainfo.go
+++ internal/data/metainfo.go
@@ -1,6 +1,9 @@
 package data
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type FileMode string
 
@@ -15,14 +18,67 @@ type info struct {
 	Private     *int64
 	FileMode    FileMode
 	Name        string
+	Length      *int64
+	Md5sum      *string
+	Files       []struct {
+		Length int64
+		Md5sum *string
+		Path   []string
+	}
 }
 
 type Metainfo struct {
-	Info         map[string]any
+	Info         info
 	Announce     string
 	AnnounceList [][]string
 	CreationData *time.Time
 	Comment      *string
 	CreatedBy    *string
 	Encoding     *string
+}
+
+func ParseMetainfo(value map[string]any) (*Metainfo, error) {
+	metainfo := Metainfo{}
+
+	announce, ok := value["announce"].(string)
+	if !ok {
+		return nil, errors.New("Missing announce")
+	}
+	metainfo.Announce = announce
+
+	announceList, err := parseAnnounceList(value)
+	if err != nil {
+		return nil, err
+	}
+	metainfo.AnnounceList = announceList
+
+	return &metainfo, nil
+}
+
+func parseAnnounceList(value map[string]any) ([][]string, error) {
+	rawAnnounceList, ok := value["announce-list"].([]any)
+	if !ok {
+		return nil, nil
+	}
+
+	al := make([][]string, 0)
+	for _, announceSubList := range rawAnnounceList {
+		switch a := announceSubList.(type) {
+		case []any:
+			subList := make([]string, 0)
+			for _, announceValues := range a {
+				switch v := announceValues.(type) {
+				case string:
+					subList = append(subList, v)
+				default:
+					return nil, errors.New("error malformed announce")
+				}
+			}
+			al = append(al, subList)
+		default:
+			return nil, errors.New("error malformed announce")
+		}
+	}
+
+	return al, nil
 }
